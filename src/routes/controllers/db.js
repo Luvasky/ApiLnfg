88e3e7333f0372dce307190e0519a87964296547
polakohernandez <luvaskyhramirez@gmail.com>
2023-11-17 07:06:43 -0500

    Insertando nuevamente las vaiables de entorno

--- src/routes/controllers/db.js
+++ src/routes/controllers/db.js
@@ -8,9 +8,9 @@ import {
 } from "../../config.js";
 
 export const pool = createPool({
-  host: "localhost",
-  user: "root",
-  password: "1065828184",
-  port: 3306,
-  database: "lnfg",
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  port: DB_PORT,
+  database: DB_NAME,
 });
